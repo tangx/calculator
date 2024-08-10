9d3eddf1a3d5373319471a980c5d7939334f7556
Frank (Xin) Tang <f.tang@aligntech.com>
2024-08-10 13:18:16 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const [numbers, setNumbers] = useState(getNumbers())
+
+  const [n, setNumbers] = useState(getNumbers())
+
+  useEffect(() => {
+    setNumbers(getNumbers())
+  }, [])
+
+
+  const refreshNumbers = () => {
+    setNumbers(getNumbers())
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{n.a}+{n.b}={n.c}</h1>
+      <button onClick={refreshNumbers}>Refresh</button>
     </>
   )
 }
@@ -39,15 +28,14 @@ export default App
 
 function getNumbers() {
 
-  const a = getRandInt(10)
-  const b = getRandInt(10)
-  const c = a + b
-  if (c <= 10) {
-    return { a, b, c }
-  } else {
-    getNumbers()
+  for (; ;) {
+    const a = getRandInt(10)
+    const b = getRandInt(10)
+    const c = a + b
+    if (c <= 10) {
+      return { a, b, c }
+    }
   }
-
 }
 
 function getRandInt(max) {
